9d3d41be05a5869a5cfe0ba7fa9d0d95cfdbf852
gaqp <gaqp@cin.ufpe.br>
2020-08-25 20:41:19 -0300

    Updates

---
+++ precache-manifest.346bc95f418a03655f27d0632238474a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9bb578c9d647dc9457e92314a69cb6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80d9d635049f18a3817",
+    "url": "/static/css/main.1481af18.chunk.css"
+  },
+  {
+    "revision": "5aecb7fa359475711ee0",
+    "url": "/static/js/2.25a8177c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25a8177c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80d9d635049f18a3817",
+    "url": "/static/js/main.03e4090b.chunk.js"
+  },
+  {
+    "revision": "bb3b86afdcb804f79e5f",
+    "url": "/static/js/runtime-main.bd0118da.js"
+  },
+  {
+    "revision": "12e55ad55fc98697ba5140f6a967d4fc",
+    "url": "/static/media/pokedolar.12e55ad5.png"
+  }
+]);

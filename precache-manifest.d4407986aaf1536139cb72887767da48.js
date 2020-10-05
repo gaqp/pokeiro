5f3331e654e29423e497fd3b012dc3e54b196184
gaqp <gaqp@cin.ufpe.br>
2020-10-05 11:20:36 -0300

    Updates

---
+++ precache-manifest.d4407986aaf1536139cb72887767da48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb1576513ecf9d7df7ea8b0c8a6176d",
+    "url": "/pokeiro/index.html"
+  },
+  {
+    "revision": "021ee4ad46857af9951f",
+    "url": "/pokeiro/static/css/main.2a60f5b3.chunk.css"
+  },
+  {
+    "revision": "5aecb7fa359475711ee0",
+    "url": "/pokeiro/static/js/2.25a8177c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokeiro/static/js/2.25a8177c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "021ee4ad46857af9951f",
+    "url": "/pokeiro/static/js/main.8dad7b03.chunk.js"
+  },
+  {
+    "revision": "60feadaff06cd5dc85e2",
+    "url": "/pokeiro/static/js/runtime-main.5e713b30.js"
+  },
+  {
+    "revision": "12e55ad55fc98697ba5140f6a967d4fc",
+    "url": "/pokeiro/static/media/pokedolar.12e55ad5.png"
+  }
+]);
